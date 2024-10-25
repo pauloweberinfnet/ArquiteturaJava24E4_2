b5c5dc156cdd6f1ccc666537ad03993915abf53c
pauloweberinfnet <paulo.weber@al.infnet.edu.br>
2024-10-25 18:14:02 -0300

    Initial Eletric class addition.

---
+++ src/main/java/br/edu/infnet/pauloweber/model/domain/Eletric.java
@@ -0,0 +1,27 @@
+package br.edu.infnet.pauloweber.model.domain;
+
+public class Eletric extends Engine{
+	private String batteryNominalCapacity;
+  private float batteryCurrentCapacity;
+  private float batteryHealth;
+
+  public String getBatteryNominalCapacity() {
+    return batteryNominalCapacity;
+  }
+  public void setBatteryNominalCapacity(String batteryNominalCapacity) {
+    this.batteryNominalCapacity = batteryNominalCapacity;
+  }
+  public float getBatteryCurrentCapacity() {
+    return batteryCurrentCapacity;
+  }
+  public void setBatteryCurrentCapacity(float batteryCurrentCapacity) {
+    this.batteryCurrentCapacity = batteryCurrentCapacity;
+  }
+	public float getBatteryHealth() {
+    return batteryHealth;
+  }
+  public void setBatteryHealth(float batteryHealth) {
+    this.batteryHealth = batteryHealth;
+  }
+
+}
