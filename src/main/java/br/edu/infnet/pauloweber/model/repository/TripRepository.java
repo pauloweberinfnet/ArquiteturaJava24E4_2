bbc3d31f68c41b97b18cce2ead1919a42ddf6f40
pauloweberinfnet <paulo.weber@al.infnet.edu.br>
2024-11-09 18:21:18 -0300

    Added Trip Service and Repository content.

--- src/main/java/br/edu/infnet/pauloweber/model/repository/TripRepository.java
+++ src/main/java/br/edu/infnet/pauloweber/model/repository/TripRepository.java
@@ -1,5 +1,19 @@
 package br.edu.infnet.pauloweber.model.repository;
 
-public interface TripRepository {
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.edu.infnet.pauloweber.model.domain.Trip;
+
+public interface TripRepository extends CrudRepository<Trip, Integer> {
+
+  List<Trip> findByDriverId(Integer driverId);
+
+  List<Trip> findByDateBetween(LocalDateTime startDate, LocalDateTime endDate);
+
+  List<Trip> findByVehicleId(Integer vehicleId);
+
 
 }
--- src/main/java/br/edu/infnet/pauloweber/model/service/TripService.java
+++ src/main/java/br/edu/infnet/pauloweber/model/service/TripService.java
@@ -1,5 +1,30 @@
 package br.edu.infnet.pauloweber.model.service;
 
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.edu.infnet.pauloweber.model.domain.Trip;
+import br.edu.infnet.pauloweber.model.repository.TripRepository;
+
 public class TripService {
+  @Autowired
+  private TripRepository tripRepository;
+
+  public Trip add(Trip trip) {
+    return tripRepository.save(trip);
+  }
+
+  public void remove(Integer id) {
+    tripRepository.deleteById(id);
+  }
+
+  public Collection<Trip> getAll() {
+    return (Collection<Trip>) tripRepository.findAll();
+  }
+
+  public Collection<Trip> getAllByDriverId(Integer driverId) {
+    return tripRepository.findByDriverId(driverId);
+  }
 
 }
