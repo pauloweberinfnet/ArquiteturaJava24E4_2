77ac94845a571389e0818de50b99acf38c3c3353
pauloweberinfnet <paulo.weber@al.infnet.edu.br>
2024-11-16 19:05:07 -0300

    Completion of Combustion service and repository.

--- src/main/java/br/edu/infnet/pauloweber/model/repository/CombustionRepository.java
+++ src/main/java/br/edu/infnet/pauloweber/model/repository/CombustionRepository.java
@@ -1,5 +1,9 @@
 package br.edu.infnet.pauloweber.model.repository;
 
-public interface CombustionRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import br.edu.infnet.pauloweber.model.domain.Combustion;
+
+public interface CombustionRepository extends CrudRepository<Combustion, Integer> {
 
 }
--- src/main/java/br/edu/infnet/pauloweber/model/service/CombustionService.java
+++ src/main/java/br/edu/infnet/pauloweber/model/service/CombustionService.java
@@ -1,5 +1,26 @@
 package br.edu.infnet.pauloweber.model.service;
 
-public class CombustionService {
+import java.util.Collection;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.edu.infnet.pauloweber.model.domain.Combustion;
+import br.edu.infnet.pauloweber.model.repository.CombustionRepository;
+
+@Service
+public class CombustionService {
+  @Autowired
+  private CombustionRepository combustionRepository;
+
+  public Combustion add(Combustion combustion) {
+    return combustionRepository.save(combustion);
+  }
+
+  public long count() {
+    return combustionRepository.count();
+  }
+  public Collection<Combustion> getAll() {
+    return (Collection<Combustion>) combustionRepository.findAll();
+  }
 }
