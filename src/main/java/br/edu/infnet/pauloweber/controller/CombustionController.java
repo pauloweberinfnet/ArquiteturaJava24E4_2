b26afb5612bda09710061c697eb2043ada9f8dc9
pauloweberinfnet <paulo.weber@al.infnet.edu.br>
2024-11-17 23:58:54 -0300

    Addition of Eletric and Combustion RestController.

---
+++ src/main/java/br/edu/infnet/pauloweber/controller/CombustionController.java
@@ -0,0 +1,36 @@
+package br.edu.infnet.pauloweber.controller;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.infnet.pauloweber.model.domain.Combustion;
+import br.edu.infnet.pauloweber.model.service.CombustionService;
+
+@RestController
+@RequestMapping("/api/vehicles/combustion")
+public class CombustionController {
+
+    @Autowired
+    private CombustionService combustionService;
+
+    @GetMapping
+    public Collection<Combustion> getAll() {
+        return combustionService.getAll();
+    }
+
+    @GetMapping("/count")
+    public long getCount() {
+        return combustionService.count();
+    }
+
+    @PostMapping
+    public Combustion add(@RequestBody Combustion combustion) {
+        return combustionService.add(combustion);
+    }
+}
---
+++ src/main/java/br/edu/infnet/pauloweber/controller/EletricController.java
@@ -0,0 +1,36 @@
+package br.edu.infnet.pauloweber.controller;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.infnet.pauloweber.model.domain.Eletric;
+import br.edu.infnet.pauloweber.model.service.EletricService;
+
+@RestController
+@RequestMapping("/api/vehicles/eletric")
+public class EletricController {
+
+    @Autowired
+    private EletricService eletricService;
+
+    @GetMapping
+    public Collection<Eletric> getAll() {
+        return eletricService.getAll();
+    }
+
+    @GetMapping("/count")
+    public long getCount() {
+        return eletricService.count();
+    }
+
+    @PostMapping
+    public Eletric add(@RequestBody Eletric eletric) {
+        return eletricService.add(eletric);
+    }
+}
